6679f89e9699cac58d49e39ecfecef8161c474db
William <billy.ryan.rom@gmail.com>
2020-07-27 13:55:48 -0400

    Add essential variables for game-of-life

--- game-of-life/src/App.tsx
+++ game-of-life/src/App.tsx
@@ -1,2 +1,16 @@
 import React, { useState, useCallback, useRef } from 'react';
 import produce from "immer"
+
+const numRows = 50;
+const numCols = 50;
+
+const operations = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0]
+];
